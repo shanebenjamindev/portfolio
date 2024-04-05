0abc8f3cd0dbddcf297264f741cb007e8f3c224a
Your Name <you@example.com>
2024-04-05 14:04:06 +0700

    added frontend cv

--- src/components/DownloadCV/index.jsx
+++ src/components/DownloadCV/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function DownloadCV() {
   return (
-    <Link to="https://drive.usercontent.google.com/download?id=10z7WvEllwd1tfmkpndcjnsPeaF0iL83M&export=download&authuser=1&confirm=t&uuid=069751fc-38e8-4f05-a8c3-0d39bbc37919&at=APZUnTX1QCEJmTgusHtdt-ME162g:1712300348330">
+    <Link to="https://drive.usercontent.google.com/download?id=1bkuv2-M1ORz6Gj7qLXcM1DUSUUbRsZJH&export=download&authuser=1&confirm=t&uuid=939e9c82-1455-41c5-8a8b-6d69944bf61e&at=APZUnTXqDekvyDZeW0R_UT9ONThS:1712300620161">
       <button className="btn__Primary">Download CV</button>
     </Link>
   );
